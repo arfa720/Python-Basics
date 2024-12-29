8086e437f1d34d0676477ec4ceede3e68ce1dff7
Muhammad Arfa <129337578+arfa720@users.noreply.github.com>
2024-12-29 23:03:02 +0500

    Add files via upload

--- 09_File_Handling/09_File_Handling.py
+++ 09_File_Handling/09_File_Handling.py
@@ -1,1 +1,10 @@
+# File Handling in Python
 
+# Writing to a file
+with open("sample.txt", "w") as file:
+    file.write("Hello, File Handling!")
+
+# Reading from a file
+with open("sample.txt", "r") as file:
+    content = file.read()
+    print("File Content:", content)
