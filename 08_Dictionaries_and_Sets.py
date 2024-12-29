91c1e03750b3d5354fc3db5e630090239ab858fa
Muhammad Arfa <129337578+arfa720@users.noreply.github.com>
2024-12-29 22:53:51 +0500

    Delete 08_Dictionaries_and_Sets.py

--- 08_Dictionaries_and_Sets.py
+++
@@ -1,9 +0,0 @@
-# Dictionaries and Sets
-
-# Dictionary
-person = {"name": "Alice", "age": 25}
-print("Name:", person["name"])
-
-# Set
-unique_numbers = {1, 2, 3, 3}
-print("Unique Numbers:", unique_numbers)
