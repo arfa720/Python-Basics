34db48dc4afe3d2ae25e13c112efcb63c734bd49
Muhammad Arfa <129337578+arfa720@users.noreply.github.com>
2024-12-29 23:02:45 +0500

    Add files via upload

--- 08_Dictionaries_and_Sets/08_Dictionaries_and_Sets.py
+++ 08_Dictionaries_and_Sets/08_Dictionaries_and_Sets.py
@@ -1,1 +1,9 @@
+# Dictionaries and Sets
 
+# Dictionary
+person = {"name": "Alice", "age": 25}
+print("Name:", person["name"])
+
+# Set
+unique_numbers = {1, 2, 3, 3}
+print("Unique Numbers:", unique_numbers)
